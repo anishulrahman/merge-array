a0cd5decd7a446ebe46266d00a03c515c90437ec
anishulrahman <anishulrahman7075@gmail.com>
2025-01-07 17:14:50 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int array1[20];
+	int array2[20];
+	int array3[20];
+	int i;
+	int s1;
+	printf("enter the size of the array1");
+	scanf("%d",&s1);
+	for(i=0;i<s1;i++){
+		printf("enter a the element :");
+		scanf("%d",&array1[i]);
+	}
+	int s2;
+	printf("enter the size of the array2");
+	scanf("%d",&s2);
+		for(i=0;i<s2;i++){
+		printf("enter a the element :");
+		scanf("%d",&array2[i]);
+	}
+		for(i=0;i<s1;i++){
+		array3[i]=array1[i];
+	}
+		for(i=0;i<s2;i++){
+		array3[s1+i]=array2[i];
+	}
+		for(i=0;i<s1+s2;i++){
+		printf("%d\n",array3[i]);
+	}
+}
